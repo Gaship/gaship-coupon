00b01ef92cce98c8ae24b03ea8da4a5c5c3b1fe4
gyeomjunchoi <gbeovhsqhtka@naver.com>
2022-08-12 06:47:39 +0900

    [feat✨] : coupontype enitty 추가

---
+++ src/test/java/shop/gaship/coupon/coupontype/CouponType.java
@@ -0,0 +1,22 @@
+package shop.gaship.coupon.coupontype;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * @author : 최겸준
+ * @since 1.0
+ */
+@Entity
+@Table(name = "coupon_types")
+public class CouponType {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "coupone_type_no")
+    private Integer couponTypeNo;
+
+}
